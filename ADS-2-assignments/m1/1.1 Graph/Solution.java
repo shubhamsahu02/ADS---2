78a6d4b73b1cd73ff42c9bbe6c12cb135cd17b89
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-11-05 00:12:58 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m1/1.1 Graph/Solution.java
+++ ADS-2-assignments/m1/1.1 Graph/Solution.java
@@ -23,7 +23,7 @@ public final class Solution {
         int vertices = scan.nextInt();
         int edges = scan.nextInt();
         scan.nextLine();
-        LinearProbingHashST<Integer, String> hashst = new 
+        LinearProbingHashST<Integer, String> hashst = new
         LinearProbingHashST<>();
         String[] places = scan.nextLine().split(",");
         for (int i = 0; i < places.length; i++) {
@@ -34,10 +34,10 @@ public final class Solution {
             Graph graph = new Graph(vertices);
             while (scan.hasNext()) {
                 String[] tokens = scan.nextLine().split(" ");
-                graph.addEdge(Integer.parseInt(tokens[0]), 
+                graph.addEdge(Integer.parseInt(tokens[0]),
                     Integer.parseInt(tokens[1]));
             }
-            System.out.println(graph.numberofVertices() + " vertices, " + 
+            System.out.println(graph.numberofVertices() + " vertices, " +
                 graph.numberofEdges() + " edges");
             if (graph.numberofEdges() == 0) {
                 System.out.println("No edges");
@@ -56,10 +56,10 @@ public final class Solution {
             AdjMatrixGraph matrixgraph = new AdjMatrixGraph(vertices);
             while (scan.hasNext()) {
                 String[] tokens1 = scan.nextLine().split(" ");
-                matrixgraph.addEdge(Integer.parseInt(tokens1[0]), 
+                matrixgraph.addEdge(Integer.parseInt(tokens1[0]),
                     Integer.parseInt(tokens1[1]));
             }
-            System.out.println(matrixgraph.numberofVertices() + " vertices, " 
+            System.out.println(matrixgraph.numberofVertices() + " vertices, "
                 + matrixgraph.numberofEdges() + " edges");
             if (matrixgraph.numberofEdges() == 0) {
                 System.out.println("No edges");
