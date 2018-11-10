05406c96a72296eb055cf049aff8c32357794503
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-11-10 12:35:00 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -20,7 +20,7 @@ public class Solution {
 		String caseToGo = scan.nextLine();
 		switch (caseToGo) {
 		case "Graph":
-			//Print the Graph Object.
+			//Prints the Graph Object.
 
 			System.out.println(edgeweight);
 			break;
@@ -30,12 +30,18 @@ public class Solution {
 			// First is the source and second is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
+			String[] tokens = scan.nextLine().split(" ");
+			int source = Integer.parseInt(tokens[0]);
+			int destination = Integer.parseInt(tokens[2]);
+   	        DijkstraUndirectedSP dijkstraObj = new DijkstraUndirectedSP(edgeweight, source);
+   	        double distBetween = dijkstraObj.distTo(destination);
+   	        System.out.println(distBetween);
 			break;
 
 		case "ViaPaths":
 			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
+			// First is the source and second is the via is the one where path should pass through.
+			// Third is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
 			break;
