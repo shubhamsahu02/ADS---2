7013483a70685c9de0980afb9631716be0e3561a
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-11-10 12:54:21 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
+++ ADS-2-assignments/m12/ADS-2_Exam-2/Solution.java
@@ -5,73 +5,76 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
 /**
   * Constructs the object.
   */
-	private Solution() {
+    private Solution() {
     // unused constructor
     }
-    
+
     /**
      * { function_description }.
      *
      * @param      args  The arguments
      */
 
-	public static void main(String[] args) {
-		// Self loops are not allowed...
-		// Parallel Edges are allowed...
-		// Take the Graph input here...
-		Scanner scan = new Scanner(System.in);
-		int vertexCount = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		EdgeWeightedGraph edgeweight = new EdgeWeightedGraph(vertexCount);
-		for (int i = 0; i < edges; i++) {
-			String[] tokens = scan.nextLine().split(" ");
-			int vert = Integer.parseInt(tokens[0]);
-			int edg = Integer.parseInt(tokens[1]);
-			double weight = Double.parseDouble(tokens[2]);
-			Edge edgeObj = new Edge(vert, edg, weight);
-			edgeweight.addEdge(edgeObj);
-		}
-		String caseToGo = scan.nextLine();
-		switch (caseToGo) {
-		case "Graph":
-			//Print the Graph Object.
+    public static void main(final String[]  args) {
+        // Self loops are not allowed...
+        // Parallel Edges are allowed...
+        // Take the Graph input here...
+        Scanner scan = new Scanner(System.in);
+        int vertexCount = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        EdgeWeightedGraph edgeweight =
+         new EdgeWeightedGraph(vertexCount);
+        for (int i = 0; i < edges; i++) {
+            String[] tokens = scan.nextLine().split(" ");
+            int vert = Integer.parseInt(tokens[0]);
+            int edg = Integer.parseInt(tokens[1]);
+            double weight = Double.parseDouble(tokens[2]);
+            Edge edgeObj = new Edge(vert, edg, weight);
+            edgeweight.addEdge(edgeObj);
+        }
+        String caseToGo = scan.nextLine();
+        switch (caseToGo) {
+        case "Graph":
+            //Print the Graph Object.
 
-			System.out.println(edgeweight);
-			break;
+            System.out.println(edgeweight);
+            break;
 
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			String[] tokens = scan.nextLine().split(" ");
-			int source = Integer.parseInt(tokens[0]);
-			int destination = Integer.parseInt(tokens[1]);
-   	        DijkstraUndirectedSP dijkstraObj = new DijkstraUndirectedSP(edgeweight, source);
-   	        double distBetween = dijkstraObj.distTo(destination);
-   	        if (dijkstraObj.hasPathTo(destination)) {
-   	        	System.out.println(distBetween);
-   	        } else {
-   	        	System.out.println("No Path Found.");
-   	        }
-   	        // System.out.println(distBetween);
-			break;
+        case "DirectedPaths":
+            // Handle the case of DirectedPaths,
+            //where two integers are given.
+            // First is the source and second is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            String[] tokens = scan.nextLine().split(" ");
+            int source = Integer.parseInt(tokens[0]);
+            int destination = Integer.parseInt(tokens[1]);
+            DijkstraUndirectedSP dijkstraObj =
+             new DijkstraUndirectedSP(edgeweight, source);
+            double distBetween = dijkstraObj.distTo(destination);
+            if (dijkstraObj.hasPathTo(destination)) {
+                System.out.println(distBetween);
+            } else {
+                System.out.println("No Path Found.");
+            }
+            // System.out.println(distBetween);
+            break;
 
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			break;
+        case "ViaPaths":
+            // Handle the case of ViaPaths, where three integers are given.
+            // First is the source and second is the via is the one where path should pass through.
+            // Third is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            break;
 
-		default:
-			break;
-		}
+        default:
+            break;
+        }
 
-	}
+    }
 }
