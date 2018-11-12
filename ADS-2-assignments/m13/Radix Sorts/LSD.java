436f2ef9f1da3d2f33cbc57d8ab7b6ebb9d9a3ed
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-11-12 21:19:09 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2-assignments/m13/Radix Sorts/LSD.java
@@ -0,0 +1,55 @@
+public class LSD {
+    private static final int BITS_PER_BYTE = 8;
+
+    // do not instantiate
+    public LSD() { }
+    public static String[] result;
+
+   /**  
+     * Rearranges the array of W-character strings in ascending order.
+     *
+     * @param a the array to be sorted
+     * @param w the number of characters per string
+     */
+    public static void sort(String[] a, int w) {
+        int n = a.length;
+        result = a;
+        int R = 256;   // extend ASCII alphabet size
+        String[] aux = new String[n];
+
+        for (int d = w-1; d >= 0; d--) {
+            // sort by key-indexed counting on dth character
+
+            // compute frequency counts
+            int[] count = new int[R+1];
+            for (int i = 0; i < n; i++)
+                count[a[i].charAt(d) + 1]++;
+
+            // compute cumulates
+            for (int r = 0; r < R; r++)
+                count[r+1] += count[r];
+
+            // move data
+            for (int i = 0; i < n; i++)
+                aux[count[a[i].charAt(d)]++] = a[i];
+
+            // copy back
+            for (int i = 0; i < n; i++)
+                a[i] = aux[i];
+            result = a;
+        }
+    }
+    /**
+     * toString method.
+     * @return string.
+     */
+    public String toString() {
+        String str = "[";
+        for (int i = 0; i < result.length - 1; i++) {
+            str += result[i] + ", ";
+        }
+        str += result[result.length - 1] + "]";
+        return str;
+    }
+
+    }
---
+++ ADS-2-assignments/m13/Radix Sorts/Solution.java
@@ -0,0 +1,34 @@
+/**.
+ * imports Scanner package
+ */
+import java.util.Scanner;
+/**.
+ * Class for solution.
+ */
+final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**.
+     * main method to perform operations.
+     * Time complexity of this method is O(E).
+     * E is number of edges.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numberof = Integer.parseInt(scan.nextLine());
+        String[] a = new String[numberof];
+        for (int i =0; i < numberof; i++) {
+            a[i] = scan.nextLine();
+        }
+        int b = a[0].length();
+        LSD lsd = new LSD();
+        lsd.sort(a, b);
+        System.out.println(lsd);
+    }
+}
