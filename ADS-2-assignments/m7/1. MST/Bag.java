2fbcad07ed6dc9c6e56c642dd5642ba304cba67b
shubhamsahu02 <41899451+shubhamsahu02@users.noreply.github.com>
2018-11-23 16:14:46 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m7/1. MST/Bag.java
+++ ADS-2-assignments/m7/1. MST/Bag.java
@@ -1,150 +1,111 @@
-/******************************************************************************
- *  Compilation:  javac Bag.java
- *  Execution:    java Bag < input.txt
- *  Dependencies: StdIn.java StdOut.java
- *
- *  A generic bag or multiset, implemented using a singly-linked list.
- *
- *  % more tobe.txt 
- *  to be or not to - be - - that - - - is
- *
- *  % java Bag < tobe.txt
- *  size of bag = 14
- *  is
- *  -
- *  -
- *  -
- *  that
- *  -
- *  -
- *  be
- *  -
- *  to
- *  not
- *  or
- *  be
- *  to
- *
- ******************************************************************************/
-
+/**.
+ * imports iterator packagae
+ */
 import java.util.Iterator;
+/**.
+ * imports nosuchelementexception
+ */
 import java.util.NoSuchElementException;
-
-/**
- *  The {@code Bag} class represents a bag (or multiset) of 
- *  generic items. It supports insertion and iterating over the 
- *  items in arbitrary order.
- *  <p>
- *  This implementation uses a singly-linked list with a static nested class Node.
- *  See {@link LinkedBag} for the version from the
- *  textbook that uses a non-static nested class.
- *  See {@link ResizingArrayBag} for a version that uses a resizing array.
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em> operations
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+/**.
+ * Class for bag.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  @param <Item> the generic type of an item in this bag
+ * @param      <Item>  The item
  */
 public class Bag<Item> implements Iterable<Item> {
-    private Node<Item> first;    // beginning of bag
-    private int n;               // number of elements in bag
-
-    // helper linked list class
-    private static class Node<Item> {
+    /**.
+     * number of elements in bag
+     */
+    private int numberofelementinbag;
+    /**.
+     * beginning of bag
+     */
+    private Node first;
+    /**.
+     * Class for node.
+     */
+    private class Node {
+        /**.
+         * item of type item
+         */
         private Item item;
-        private Node<Item> next;
+        /**.
+         * next of NOde type
+         */
+        private Node next;
     }
 
-    /**
-     * Initializes an empty bag.
-     */
+    /**.
+      * Create an empty stack.
+      */
     public Bag() {
         first = null;
-        n = 0;
+        numberofelementinbag = 0;
     }
 
-    /**
-     * Returns true if this bag is empty.
-     *
-     * @return {@code true} if this bag is empty;
-     *         {@code false} otherwise
-     */
+    /**.
+      * Is the BAG empty?
+      * Time complesxity is constant as each statement is executed only once
+      * @return is boolean type
+      */
     public boolean isEmpty() {
         return first == null;
     }
 
-    /**
-     * Returns the number of items in this bag.
-     *
-     * @return the number of items in this bag
-     */
+    /**.
+      * Return the number of items in the bag.
+      * Time complesxity is constant as each statement is executed only once
+      * @return returns size of bag of int type
+      */
     public int size() {
-        return n;
+        return numberofelementinbag;
     }
-
-    /**
-     * Adds the item to this bag.
-     *
-     * @param  item the item to add to this bag
-     */
-    public void add(Item item) {
-        Node<Item> oldfirst = first;
-        first = new Node<Item>();
+    /**.
+      * Add the item to the bag.
+      * Time complesxity is constant as each statement is executed only once
+      *@param item of item type
+      */
+    public void add(final Item item) {
+        Node oldfirst = first;
+        first = new Node();
         first.item = item;
         first.next = oldfirst;
-        n++;
+        numberofelementinbag++;
     }
-
-
-    /**
-     * Returns an iterator that iterates over the items in this bag in arbitrary order.
-     *
-     * @return an iterator that iterates over the items in this bag in arbitrary order
-     */
+    /**.
+      * Return an iterator that iterates over the items in the bag.
+      * @return Listiterator
+      */
     public Iterator<Item> iterator()  {
-        return new ListIterator<Item>(first);  
+        return new ListIterator();
     }
-
-    // an iterator, doesn't implement remove() since it's optional
-    private class ListIterator<Item> implements Iterator<Item> {
-        private Node<Item> current;
-
-        public ListIterator(Node<Item> first) {
-            current = first;
+    /**.
+     * Class for list iterator.
+     */
+    private class ListIterator implements Iterator<Item> {
+        /**.
+         * current of Node type
+         */
+        private Node current = first;
+        /**.
+         * Determines if it has next.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext()  {
+            return current != null;
         }
-
-        public boolean hasNext()  { return current != null;                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**.
+         * checks if next vertex is there or not
+         *
+         * @return     { returns item of ITem type }
+         */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
-
-    /**
-     * Unit tests the {@code Bag} data type.
-     *
-     * @param args the command-line arguments
-     */
-/*    public static void main(String[] args) {
-        Bag<String> bag = new Bag<String>();
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            bag.add(item);
-        }
-
-        StdOut.println("size of bag = " + bag.size());
-        for (String s : bag) {
-            StdOut.println(s);
-        }
-    }*/
-
 }
